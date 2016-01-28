86044f9fe78d45cc469745ce0db6ada0a85fff5a
willibaur <williambautista@gmail.com>
2016-01-28 14:07:26 +0000

    Find profile test changed, element.all added

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -2,7 +2,7 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs:           ['githubProfileFeature.js'],
   // capabilities:    { browserName: 'firefox' }
-  multicapabilities: [
+  multiCapabilities: [
                       { browserName: 'firefox' },
                       { browserName: 'chrome' }
   ]
