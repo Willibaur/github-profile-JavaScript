543f86911f9138d2284d951ee9182f6bf947f907
willibaur <williambautista@gmail.com>
2016-01-28 16:42:53 +0000

    Mock API added

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -1,9 +1,22 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
+
   specs:           ['githubProfileFeature.js'],
   // capabilities:    { browserName: 'firefox' }
   multiCapabilities: [
                       { browserName: 'firefox' },
                       { browserName: 'chrome' }
-  ]
+  ],
+
+  mocks: {
+            default: [], //default value: []
+            dir:     'mocks'  // default value: 'mocks'
+  },
+
+  onPrepare: function () {
+    require('protractor-http-mock').config = {
+      rootDirectory:__dirname,  // default value: process.cwd()
+      protractorConfig: 'conf.js' // default value: 'protractor.conf'
+    };
+  }
 };
--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,10 +1,17 @@
+var mock = require('protractor-http-mock');
+
 describe('GitHub profile finder', function() {
 
   var searchBox = element(by.model('searchCtrl.searchTerm'));
   var searchButton = element(by.className('btn'));
 
   beforeEach(function() {
+  	mock(['githubUserSearch.js']);
     browser.get('http://localhost:8080');
+  });
+
+  afterEach(function() {
+  	mock.teardown();
   });
 
   it('has a title', function() {
@@ -12,10 +19,10 @@ describe('GitHub profile finder', function() {
   });
 
   it('finds profiles', function() {
-    searchBox.sendKeys('spike');
+    searchBox.sendKeys('ptolemy');
     searchButton.click();
 
     var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-    expect(profiles.last().getText()).toEqual('spikebike');
+    expect(profiles.last().getText()).toEqual('ptolemy-theorem');
   });
 });
